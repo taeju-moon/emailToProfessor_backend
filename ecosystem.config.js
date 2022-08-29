69affbc0c45a47133febb6c94570d14731e1b9ee
taeju-moon <pp2lycee@naver.com>
2022-08-29 15:12:06 +0900

    a

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "index",
+      script: "./index.js",
+      instances: 0,
+      exec_mode: "cluster",
+    },
+  ],
+};
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const cors = require("cors");
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? "http://localhost:30000"
+      ? "http://localhost:3000"
       : "http://localhost:3000",
   credentials: true,
 };
